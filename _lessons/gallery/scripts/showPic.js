04125eedd044d43fa8be7a478a3e0a31a21310a4
dggomes <dg.gomes@gmail.com>
2012-04-05 17:45:21 +0100

    description updated

--- _lessons/gallery/scripts/showPic.js
+++ _lessons/gallery/scripts/showPic.js
@@ -1,7 +1,10 @@
 function showPic(whichpic){
 var source = whichpic.getAttribute("href");
 var placeholder = document.getElementById("placeholder");
+var text = whichpic.getAttribute("title");
+var description = document.getElementById("description");
 placeholder.setAttribute("src",source);
+description.firstChild.nodeValue = text;
 }
 
 
